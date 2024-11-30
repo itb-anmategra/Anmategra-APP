0e248c969454bbf1b331e9d9b368aec70b9aef2c
ARoyyanF <160403210+ARoyyanF@users.noreply.github.com>
2024-12-01 04:59:31 +0700

    feat(component): add kegiatan card for beranda

---
+++ src/app/test/components/page.tsx
@@ -0,0 +1,9 @@
+import KegiatanCard from "../../_components/beranda/kegiatanCard";
+
+export default function TestPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between gap-20 p-24">
+      <KegiatanCard />
+    </main>
+  );
+}
