a0e442fd8179e57773fe7b1575e9317ad49599ae
ARoyyanF <160403210+ARoyyanF@users.noreply.github.com>
2024-12-01 05:03:08 +0700

    feat(component): add profil mahasiswa for beranda

--- src/app/test/components/page.tsx
+++ src/app/test/components/page.tsx
@@ -1,9 +1,11 @@
 import KegiatanCard from "../../_components/beranda/kegiatanCard";
+import ProfilMahasiswa from "../../_components/beranda/profilMahasiswa";
 
 export default function TestPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-20 p-24">
       <KegiatanCard />
+      <ProfilMahasiswa />
     </main>
   );
 }
