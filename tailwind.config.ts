73f2b27f78cdd3e4d51fad155c7deb973a97ac09
Loxenary <110291644+Loxenary@users.noreply.github.com>
2024-12-06 17:51:00 +0700

    Revert "feat: Laporan Kanban Board"
    This reverts commit 2e59f6b8aa4602863438434f51ec8942a995a863.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,7 +44,6 @@ export default {
 				"secondary-800": "#006E6E",
 				"secondary-900": "#005252",
 				"secondary-1000": "#004040",
-				"secondary-1100" : "#636A6D",
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
