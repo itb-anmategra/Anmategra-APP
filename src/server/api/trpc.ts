3537bbde4e829d0b1873a2f6e14d77e520509df9
albertchriss <19623210@std.stei.itb.ac.id>
2025-01-14 14:36:57 +0700

    feat: adding trpc panel

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -87,16 +87,26 @@ export const createTRPCRouter = t.router;
 const timingMiddleware = t.middleware(async ({ next, path }) => {
   const start = Date.now();
 
-  if (t._config.isDev) {
-    // artificial delay in dev
-    const waitMs = Math.floor(Math.random() * 400) + 100;
-    await new Promise((resolve) => setTimeout(resolve, waitMs));
-  }
+  // if (t._config.isDev) {
+  //   // artificial delay in dev
+  //   const waitMs = Math.floor(Math.random() * 400) + 100;
+  //   await new Promise((resolve) => setTimeout(resolve, waitMs));
+  // }
 
   const result = await next();
 
   const end = Date.now();
   console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
+
+  // Uncomment this to include the execution time in the response
+
+  // const duration = end - start;
+  // if (result.ok) {
+  //   result.data = {
+  //     data: result.data,
+  //     executionTime: `${duration}ms`, // Include execution time in the response
+  //   };
+  // }
 
   return result;
 });
