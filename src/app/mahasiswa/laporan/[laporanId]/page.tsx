ce5f55253eea16a25ceaa345c843be103762b2bd
loxenary <zanxub@gmail.com>
2025-01-18 12:27:28 +0700

    feat: Create Laporan page for mahasiswa

---
+++ src/app/mahasiswa/laporan/[laporanId]/page.tsx
@@ -0,0 +1,42 @@
+import Router, { useRouter } from "next/router";
+
+import {
+  LaporanCard,
+  LaporanDetailProps,
+} from "~/app/_components/laporan/detail/laporan-card";
+import { SearchBar } from "~/app/_components/placeholder/search-bar";
+import { Button } from "~/components/ui/button";
+
+export default function ReportDetail() {
+  const dummyData: LaporanDetailProps = {
+    id: "1",
+    name: "Report 1",
+    date: "15/07/2024",
+    category: "Kategori",
+    content:
+      "      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac auctor mauris. Vestibulum vel pulvinar ex. Aenean in enim nisi. Morbi vitae dapibus eros, in pellentesque arcu. Integer in nunc lorem. Morbi eu quam nulla. Praesent tristique ex sed dolor laoreet dignissim. Aliquam egestas mi sed ipsum blandit egestas scelerisque elementum risus.\nNulla convallis, neque sed gravida feugiat, neque nulla consequat augue, nec mattis ex lorem consectetur felis. Curabitur fringilla, lectus non mattis molestie, ex risus varius quam, in luctus diam justo ac nulla. Vestibulum magna nulla, gravida in tellus consequat, tempus tempus quam. Nam a nunc mattis, egestas enim quis, hendrerit velit. Cras elementum nisi nec maximus gravida. Maecenas a lectus a quam feugiat rutrum. Integer posuere, sapien sit amet tempus fringilla, lorem dolor hendrerit neque, a tincidunt sapien libero sed odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur sem dolor, imperdiet ut sapien id, sodales auctor tortor.",
+    status: "In Progress",
+    attachment: [{ name: "Attachment1.pdf" }],
+  };
+
+  function backToPreviousPage() {
+    Router.back();
+  }
+
+  return (
+    <div className="flex flex-col gap-3 p-8">
+      <Button onClick={backToPreviousPage} variant={"ghost"}>
+        {"< Kembali"}
+      </Button>
+      <LaporanCard
+        status={dummyData.status}
+        content={dummyData.content}
+        id={dummyData.id}
+        name={dummyData.name}
+        date={dummyData.date}
+        category={dummyData.category}
+        attachment={dummyData.attachment}
+      />
+    </div>
+  );
+}
