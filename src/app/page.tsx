417e6e191915721bc68ec54e583284b7634cb2e0
HaydarChan <atqiyahaydar15@gmail.com>
2025-01-23 22:16:09 +0700

    feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,22 @@
-// Library Import
-import React from 'react'
-import Image from 'next/image'
-// Components Import
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card'
-import { Button } from '~/components/ui/button'
-// Asset Import
-import Logo from 'public/logo-anmategra.png'
-import Google from 'public/google.webp'
-import Microsoft from 'public/microsoft.webp'
+// Component Import
+import HeroSection from "./_components/landing/hero"; 
+import { KegiatanSection } from "./_components/landing/KegiatanSection";
+import { KepanitiaanSection } from "./_components/landing/KepanitiaanSection";
+import MahasiswaSidebar from "./_components/MahasiswaSidebar";
 
-const LogInPage = () => {
+const LandingPage = () => {
   return (
-    <div className='h-screen w-full flex items-center justify-center gap-x-4 bg-slate-100 px-4'>
-      <Card className='max-w-[450px]'>
-        <CardHeader>
-          <CardTitle className='text-2xl flex flex-col gap-y-2'>
-            <Image 
-              src={Logo}
-              width={125}
-              height={250}
-              alt='Logo Anmategra'
-            />
-            <p>Selamat Datang di <span className='bg-clip-text text-transparent bg-gradient-to-br from-primary-400 to-secondary-400 font-bold'>Anmategra</span></p>
-          </CardTitle>
-          <CardDescription>
-            Silahkan masuk dengan akun microsoft untuk mahasiswa dan akun google untuk lembaga.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className='flex flex-col gap-y-2'>
-          <Button variant={"outline"} className='flex items-center gap-x-2 transition-all hover:gap-x-4'>
-            <Image
-              src={Google}
-              alt='Logo Google'
-              width={16}
-              height={16} 
-            /> 
-            <p className='text-slate-600'>Masuk dengar Microsoft</p>
-          </Button>
-          <Button variant={"outline"} className='flex items-center gap-x-2 transition-all hover:gap-x-4'>
-            <Image 
-              src={Microsoft}
-              alt='Logo Microsoft'
-              width={14}
-              height={14}
-            />
-            <p className='text-slate-600'>Masuk dengan Google</p>
-          </Button>
-        </CardContent>
-      </Card>
-    </div>
-  )
-}
+    <main className="flex flex-col overflow-hidden pb-16 sm:space-y-4 md:space-y-8">
+      <div className="mb-12 fixed w-full shadow-sm z-20">
+        <MahasiswaSidebar />
+      </div>
+      <HeroSection />
+      <div className="space-y-16">
+        <KepanitiaanSection />
+        <KegiatanSection />
+      </div>
+    </main>
+  );
+};
 
-export default LogInPage
+export default LandingPage;
