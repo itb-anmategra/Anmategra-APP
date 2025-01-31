587af282b6e97291d3012fbfb6406e7e42d48009
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2025-01-31 20:35:47 +0700

    fix : Kegiatan Page

--- src/app/lembaga/kegiatan/page.tsx
+++ src/app/lembaga/kegiatan/page.tsx
@@ -24,39 +24,7 @@ export default async function Home() {
 
   return (
     <main className="flex flex-row bg-[#FAFAFA] w-full p-6">
-      {/* Content */}
-      <div className="flex-1 space-y-8">
-        {/* Search Bar */}
-        <div className="w-full">
-          <p className="text-2xl mb-4 font-semibold">Kegiatan</p>
-          <div className="flex items-center gap-4">
-            {/* Search Bar */}
-            <div className="relative w-full">
-              <Image
-                src={SearchIcon}
-                alt="Search"
-                width={20}
-                height={20}
-                className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400"
-              />
-              <Input
-                placeholder="Cari nama anggota"
-                className="rounded-2xl bg-white placeholder:text-neutral-700 focus-visible:ring-transparent"
-                startAdornment={
-                  <MagnifyingGlassIcon className="size-4 text-gray-500" />
-                }
-              />
-            </div>
-          </div>
-        </div>
-
-        {/* List Kegiatan */}
-        <div>
-          <div>
-            <ActivityList propActivites={formattedActivities}/>
-          </div>
-        </div>
-      </div>
+        <ActivityList propActivites={formattedActivities}/>
     </main>
   );
 }
