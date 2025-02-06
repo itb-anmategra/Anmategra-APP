8f8d464b4a757095c8a825bcc705356f5ac8db25
HaydarChan <atqiyahaydar15@gmail.com>
2025-02-06 21:23:46 +0700

    feat: add profile pages for lembaga layout

---
+++ src/app/lembaga/profil-lembaga/[lembagaId]/page.tsx
@@ -0,0 +1,138 @@
+// Libray Import
+import React from 'react'
+import Image from 'next/image'
+// Asset Import
+import DummyFotoLembaga from 'public/logo-hmif.png'
+import DummyFotoEvent from 'public/placeholder/kegiatan thumbnail.png'
+import LogoHMIFKecil from 'public/placeholder/logo if.png'
+// Components Import
+import { Card } from '~/components/ui/card'
+import { Badge } from '~/components/ui/badge'
+import { KepanitiaanCard } from '~/app/_components/beranda/KepanitiaanCard'
+import MahasiswaSidebar from "../../../_components/MahasiswaSidebar";
+// Icons Import
+import { CalendarIcon } from 'lucide-react'
+// TRPC Import
+import {api} from "~/trpc/server";
+// Auth
+import { getServerAuthSession } from '~/server/auth';
+
+const DetailLembagaPage = async (
+    {params}: {
+        params: Promise<{ lembagaId: string }>
+    }
+) => {
+    const lembagaId = (await params).lembagaId
+    const {lembagaData, newestEvent , highlightedEvent , error} = await api.profil.getLembaga({lembagaId: lembagaId})
+
+    const session = await getServerAuthSession();
+
+    if (error) {
+        return (
+            <>
+              <div className='w-full flex justify-between fixed z-20'>
+                <MahasiswaSidebar session={session?.user.id ?? ""}  />
+              </div>
+              <div className='w-full flex min-h-screen flex-col items-center pt-20'>
+                <div className="w-full max-w-7xl flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-semibold text-neutral-1000">Beranda</h1>
+                    <p>Beranda / Nama Lembaga</p>
+                    {/* <Input
+                        placeholder="Cari lembaga, kegiatan, atau mahasiswa"
+                        className="rounded-2xl bg-white focus-visible:ring-transparent placeholder:text-neutral-700"
+                        startAdornment={
+                            <MagnifyingGlassIcon className="size-4 text-gray-500"/>
+                        }
+                    /> */}
+                </div>
+                <div className="max-w-7xl flex w-full flex-col gap-4 py-6">
+                    <h1 className="text-2xl font-semibold text-neutral-1000">{error}</h1>
+                </div>
+              </div>
+            </>
+        )
+    }
+
+  return (
+    <>
+      <div className='w-full flex justify-between fixed z-20'>
+        <MahasiswaSidebar session={session?.user.id ?? ""}  />
+      </div>
+      <div className='w-full flex min-h-screen flex-col items-center pt-20'>
+        <div className="flex max-w-7xl w-full flex-col gap-4 py-6">
+        {/* Title and Search */}
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-semibold text-neutral-1000">Beranda</h1>
+          <p>Beranda / Nama Lembaga</p>
+          {/* <Input
+            placeholder="Cari lembaga, kegiatan, atau mahasiswa"
+            className="rounded-2xl bg-white focus-visible:ring-transparent placeholder:text-neutral-700"
+            startAdornment={
+              <MagnifyingGlassIcon className="size-4 text-gray-500" />
+            }
+          /> */}
+        </div>
+
+        {/* Lembaga */}
+        <div className='w-full flex items-center justify-center gap-x-6 py-12'>
+          <Image 
+            src={lembagaData?.image ?? DummyFotoLembaga}
+            alt='Foto Lembaga'
+            width={200}
+            height={100}
+          />
+          <div className='space-y-1'>
+            <p className='text-3xl text-neutral-1000 font-semibold'>{lembagaData?.name}</p>
+            <p className='text-xl text-[#8196A3]'>{lembagaData?.description}</p>
+          </div>
+        </div>
+
+        {/* Highlighted Event */}
+        <div className='space-y-4 pb-12'>
+          <h5 className='text-xl font-semibold text-neutral-1000'>Highlighed Event</h5>
+          <Card className='transition-all hover:shadow-md overflow-x-hidden flex justify-start gap-x-6 items-center'>
+            <Image 
+              src={highlightedEvent?.image ?? DummyFotoEvent}
+              alt='Foto Kegiatan'
+              className='h-full w-auto'
+              width={200}
+              height={100}
+            />
+            <div className='space-y-2'>
+              <Badge className='space-x-2 rounded-full bg-Blue-Dark py-1'>
+                <Image 
+                  src={lembagaData?.image ?? LogoHMIFKecil}
+                  alt='Logo HMIF Kecil'
+                  width={20}
+                  height={20}
+                  className='rounded-full object-cover'
+                />
+                <p className='text-xs'>{lembagaData?.name}</p>
+              </Badge>
+              <p className='text-xl text-Blue-Dark font-semibold'>{highlightedEvent?.name}</p>
+              <p className='text-neutral-1000'>{highlightedEvent?.description}</p>
+              <div className="flex items-center gap-2 text-sm text-Regent-Gray">
+                <CalendarIcon className='h-4 w-4' />
+                  {highlightedEvent?.start_date.toDateString()} -{" "}
+                  {highlightedEvent?.end_date?.toDateString()}
+              </div>
+            </div>
+          </Card>
+        </div>
+
+        {/* Kepanitiaan Terbaru */}
+        <div className='space-y-4 pb-12'>
+          <h5 className='text-xl font-semibold text-neutral-1000'>Kepanitiaan Terbaru</h5>  
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+            {newestEvent ? newestEvent.map((item) => (
+              <KepanitiaanCard kepanitiaan={item} key={item.id} />
+            )) : <p>Belum ada kepanitiaan</p>}
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default DetailLembagaPage
