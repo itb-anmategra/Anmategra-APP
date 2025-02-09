f115c4285b058004196691c79cca44d97702c4c0
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2025-02-09 23:41:57 +0700

    fix : Org_id

--- src/server/api/routers/event/update.ts
+++ src/server/api/routers/event/update.ts
@@ -1,95 +1,110 @@
-import { adminProcedure, protectedProcedure } from "../../trpc";
-import { z } from "zod";
-import {events, keanggotaan} from "~/server/db/schema";
-import { TRPCError } from "@trpc/server";
-import { eq } from "drizzle-orm";
+import {adminProcedure, protectedProcedure} from "../../trpc";
+import {z} from "zod";
+import {events, keanggotaan, lembaga} from "~/server/db/schema";
+import {TRPCError} from "@trpc/server";
+import {eq} from "drizzle-orm";
 
 export const updateEvent = protectedProcedure
-.input(
-  z.object({
-      id: z.string(),
-      name: z.string(),
-      org_id: z.string().optional(),
-      description: z.string(),
-      image: z.string(),
-      start_date: z.string().datetime(),
-      end_date: z.string().datetime(),
-      status: z.enum(["Coming Soon", "On going", "Ended"]),
-      oprec_link: z.string().url(),
-      location: z.string(),
-      participant_limit: z.number().int(),
-      participant_count: z.number().int(),
-      is_highlighted: z.boolean(),
-      is_organogram: z.boolean()
-  })
-)
-.mutation(async ({ctx, input}) => {
-  console.log("Event Update called.")
-  try {
-    const updatedEvent = await ctx.db.update(events)
-      .set({
-        ...input,
-        start_date: new Date(input.start_date),
-        end_date: new Date(input.end_date),
-      })
-      .where(eq(events.id, input.id))
-      .returning();
-  
-    return updatedEvent[0];
-  } catch (error) {
-    console.error("Database Error:", error);
+    .input(
+        z.object({
+            id: z.string(),
+            name: z.string(),
+            org_id: z.string(),
+            description: z.string(),
+            image: z.string(),
+            start_date: z.string().datetime(),
+            end_date: z.string().datetime(),
+            status: z.enum(["Coming Soon", "On going", "Ended"]),
+            oprec_link: z.string().url(),
+            location: z.string(),
+            participant_limit: z.number().int(),
+            participant_count: z.number().int(),
+            is_highlighted: z.boolean(),
+            is_organogram: z.boolean()
+        })
+    )
+    .mutation(async ({ctx, input}) => {
+        console.log("Event Update called.")
+        try {
+            const org_id = await ctx.db.query.lembaga.findFirst({
+                where: eq(lembaga.userId, input.org_id),
+                columns: {
+                    id: true
+                }
+            })
+            
+            if (!org_id) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: "Organization not found."
+                });
+            }
 
-    if (error instanceof Error) {
-      const pgError = error as { code?: string };
+            input.org_id = org_id.id;
+            const updatedEvent = await ctx.db.update(events)
+                .set({
+                    ...input,
+                    start_date: new Date(input.start_date),
+                    end_date: new Date(input.end_date)
+                })
+                .where(eq(events.id, input.id))
+                .returning();
 
-      switch (pgError.code) {
-        case '23505':
-          throw new TRPCError({
-            code: 'CONFLICT',
-            message: "A record with the same unique field already exists."
-          });
-        case '23503':
-          throw new TRPCError({
-            code: 'BAD_REQUEST',
-            message: "Invalid reference to another table."
-          });
-        case '23514':
-          throw new TRPCError({
-            code: 'BAD_REQUEST',
-            message: "Input values violate database constraints."
-          });
-      }
-    }
+            return updatedEvent[0];
+        } catch (error) {
+            console.error("Database Error:", error);
 
-    // Generic error handling
-    throw new TRPCError({
-      code: 'INTERNAL_SERVER_ERROR',
-      message: "An unexpected error occurred during event creation."
-    });
-  }
-})
+            if (error instanceof Error) {
+                const pgError = error as { code?: string };
+
+                switch (pgError.code) {
+                    case '23505':
+                        throw new TRPCError({
+                            code: 'CONFLICT',
+                            message: "A record with the same unique field already exists."
+                        });
+                    case '23503':
+                        throw new TRPCError({
+                            code: 'BAD_REQUEST',
+                            message: "Invalid reference to another table."
+                        });
+                    case '23514':
+                        throw new TRPCError({
+                            code: 'BAD_REQUEST',
+                            message: "Input values violate database constraints."
+                        });
+                }
+            }
+
+            // Generic error handling
+            throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                message: "An unexpected error occurred during event creation."
+            });
+        }
+    })
 
 export const addNewPanitia = protectedProcedure
     .input(
-      z.object({
-        event_id: z.string(),
-        user_id: z.string(),
+        z.object({
+            event_id: z.string(),
+            user_id: z.string(),
 
-      })
+        })
     )
-.mutation(async ({ctx, input}) => {
-    try {
-        const newPanitia = await ctx.db.insert(keanggotaan).values({
-            event_id: input.event_id,
-            user_id: input.user_id
-        }).returning();
+    .mutation(async ({ctx, input}) => {
+        try {
+            const newPanitia = await ctx.db.insert(keanggotaan).values({
+                event_id: input.event_id,
+                user_id: input.user_id
+            }).returning();
 
-        return newPanitia[0];
-    } catch (error) {
-        console.error("Database Error:", error);
-        throw new TRPCError({
-            code: 'INTERNAL_SERVER_ERROR',
-            message: "An unexpected error occurred during event creation."
-        });
-    }
-})
+            return newPanitia[0];
+        } catch (error) {
+            console.error("Database Error:", error);
+            throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                message: "An unexpected error occurred during event creation."
+            });
+        }
+    })
