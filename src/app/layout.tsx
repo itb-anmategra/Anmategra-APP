4345b0529cea8cf7eb8687e84c1989a68dd94acc
qiewi <105619444+qiewi@users.noreply.github.com>
2025-02-13 15:49:55 +0700

    feat: mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import {GeistSans} from "geist/font/sans";
 // Library Import
 import {type Metadata} from "next";
 import {TRPCReactProvider} from "~/trpc/react";
+import Image from "next/image";
+// Image Import
+import LogoAnmategra from "public/images/logo-anmategra.png";
+import Mascot from "public/images/mascot.png";
+import Kabinet from "public/images/kabinet.png";
 // Components Import
 import { Toaster } from "~/components/ui/toaster"
 // Metadata
@@ -24,8 +29,35 @@ export default function RootLayout({
                 {children}
             </TRPCReactProvider>
           </div>
-          <div className="w-full h-screen flex flex-col justify-center items-center lg:hidden">
-            <p className="text-secondary-400 text-xl">Silakan Buka Dalam Desktop View.</p>
+          <div className="w-full h-screen flex flex-col justify-between lg:hidden">
+            <div className="flex items-center justify-center py-12">
+              <Image 
+                src={LogoAnmategra}
+                alt="Logo Anmategra"
+                width={210}
+                height={112}
+              />
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <Image 
+                src={Mascot}
+                alt="Mascot"
+                width={240}
+                height={180}
+              />
+              <h1 className="text-[#00B7B7] text-4xl font-semibold">Website Only</h1>
+              <h2 className="text-center text-md">
+                Silahkan buka menggunakan <br/><span className="font-bold">pc / desktop</span>
+              </h2>
+            </div>
+            <div className="flex items-center justify-center py-12">
+              <Image 
+                src={Kabinet}
+                alt="Kabinet"
+                width={340}
+                height={45}
+              />
+            </div>
           </div>
           <Toaster />
         </body>
