74528294a8735a5e6c12d84ab1156c1a0a1462d6
angkaberapa <angkawijayamichael@gmail.com>
2025-09-03 00:16:44 +0700

    refactor: update lembaga router and schemas to improve input/output validation and handling

--- src/app/lembaga/anggota-kegiatan/page.tsx
+++ src/app/lembaga/anggota-kegiatan/page.tsx
@@ -7,7 +7,7 @@ import { api } from "~/trpc/server";
 
 export default async function Home() {
   const session = await getServerAuthSession();
-  const anggota = await api.lembaga.getAllAnggota({lembagaId: session?.user.id ?? ""});
+  const anggota = await api.lembaga.getAllAnggota({lembagaId: session?.user.lembagaId ?? ""});
   const addAnggotaProps = await api.users.getTambahAnggotaLembagaOptions({lembagaId: session?.user.lembagaId ?? ""});
   return (
       <main>
