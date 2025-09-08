ebb3f3de9ef053784d77dc3bdae76170e967e209
Bertha Soliany <13523026@std.stei.itb.ac.id>
2025-09-08 21:46:53 +0700

    feat: add page for lembaga

---
+++ src/app/lembaga/anggota/_components/anggota-content.tsx
@@ -0,0 +1,136 @@
+'use client';
+
+// Library Import
+// Auth Import
+import { type Session } from 'next-auth';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+// Icon Import
+import Plus from '~/../public/icons/plus.svg';
+import SearchIcon from '~/../public/icons/search.svg';
+// Import the magnifying glass icon
+import TambahAnggotaForm, {
+  type comboboxDataType,
+} from '~/app/_components/form/tambah-anggota-form';
+import TambahAnggotaKegiatanForm from '~/app/_components/form/tambah-anggota-kegiatan-form';
+import {
+  MahasiswaCardTable,
+  type Member,
+} from '~/app/lembaga/anggota/_components/table/mahasiswa-card-table';
+import { MahasiswaKegiatanCardTable } from '~/app/lembaga/anggota/_components/table/mahasiswa-kegiatan-card-table';
+import { Button } from '~/components/ui/button';
+// Components Import
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog';
+import { Input } from '~/components/ui/input';
+
+export default function AnggotaContent({
+  session,
+  data,
+  dataAddAnggota,
+}: {
+  session: Session | null;
+  data: Member[];
+  dataAddAnggota: {
+    mahasiswa: comboboxDataType[];
+    posisi: comboboxDataType[];
+    bidang: comboboxDataType[];
+  };
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const isAnggota = pathname === '/lembaga/anggota';
+  let tableData;
+  if (!isAnggota) {
+    tableData = data.map((member) => {
+      return {
+        ...member,
+        event_id: pathname.split('/')[3],
+      };
+    });
+  }
+  return (
+    <main className="flex flex-row bg-[#FAFAFA] w-full p-6">
+      {/* Content */}
+      <div className="flex-1 space-y-4">
+        {/* Search Bar */}
+        <div className="w-full">
+          <p className="text-2xl mb-4 font-semibold">
+            {isAnggota ? <span>Anggota</span> : <span>Anggota Kegiatan</span>}
+          </p>
+          <div className="flex items-center gap-4">
+            {/* Search Bar */}
+            <div className="relative w-full">
+              <Image
+                src={SearchIcon}
+                alt="Search"
+                width={20}
+                height={20}
+                className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400"
+              />
+              <Input
+                className="rounded-[24px] pl-12 pr-4 border-[1px] border-neutral-400 w-full"
+                placeholder="Cari nama anggota"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* List Anggota */}
+        <div>
+          {/* Button Section */}
+          <div className="flex justify-between">
+            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+              <DialogTrigger asChild>
+                <Button className="bg-[#00B7B7] hover:bg-[#00B7B7]/75 text-white rounded-[16px] px-4 shadow-none flex items-center gap-2">
+                  <Image
+                    src={Plus}
+                    alt="Tambah Anggota"
+                    width={24}
+                    height={24}
+                  />
+                  Tambah Anggota Baru
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Tambah Anggota</DialogTitle>
+                </DialogHeader>
+                {isAnggota ? (
+                  <TambahAnggotaForm
+                    session={session}
+                    data={dataAddAnggota}
+                    setIsOpen={setIsOpen}
+                  />
+                ) : (
+                  <TambahAnggotaKegiatanForm
+                    session={session}
+                    data={dataAddAnggota}
+                    setIsOpen={setIsOpen}
+                    pathname={pathname}
+                  />
+                )}
+              </DialogContent>
+            </Dialog>
+          </div>
+
+          {/* List Anggota Section */}
+          <div className="mt-6">
+            {/* Integrate MahasiswaCardTable here */}
+            {isAnggota ? (
+              <MahasiswaCardTable data={data} />
+            ) : (
+              <MahasiswaKegiatanCardTable data={tableData} />
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/app/lembaga/anggota/_components/table/mahasiswa-kegiatan-card-table.tsx
+++ src/app/lembaga/anggota/_components/table/mahasiswa-kegiatan-card-table.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
+
 // Library Import
-import * as React from "react";
 import {
   type ColumnDef,
   type SortingState,
@@ -9,7 +9,17 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-} from "@tanstack/react-table";
+} from '@tanstack/react-table';
+import * as React from 'react';
+import { Button } from '~/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog';
 // Components Import
 import {
   Table,
@@ -18,91 +28,94 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "~/components/ui/table";
-import { Button } from "~/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog"
+} from '~/components/ui/table';
 // TRPC Import
-import { api } from "~/trpc/react";
+import { api } from '~/trpc/react';
 
-export type Member = {
+export type MemberKegiatan = {
   id: string;
   nama: string;
   nim: string;
   divisi: string;
   posisi: string;
   posisiColor: string;
+  event_id: string;
 };
 
-const columns: ColumnDef<Member>[] = [
+const columns: ColumnDef<MemberKegiatan>[] = [
   {
-    accessorKey: "nama",
-    header: "Nama",
-    cell: ({ row }) => <span>{row.getValue("nama")}</span>,
+    accessorKey: 'nama',
+    header: 'Nama',
+    cell: ({ row }) => <span>{row.getValue('nama')}</span>,
   },
   {
-    accessorKey: "nim",
-    header: "NIM",
-    cell: ({ row }) => <span>{row.getValue("nim")}</span>,
+    accessorKey: 'nim',
+    header: 'NIM',
+    cell: ({ row }) => <span>{row.getValue('nim')}</span>,
   },
   {
-    accessorKey: "divisi",
-    header: "Divisi",
-    cell: ({ row }) => <span>{row.getValue("divisi")}</span>,
+    accessorKey: 'divisi',
+    header: 'Divisi',
+    cell: ({ row }) => <span>{row.getValue('divisi')}</span>,
   },
   {
-    accessorKey: "posisi",
-    header: "Posisi",
+    accessorKey: 'posisi',
+    header: 'Posisi',
     cell: ({ row }) => {
       const colorMap: Record<string, string> = {
-        yellow: "bg-yellow-400",
-        green: "bg-green-400",
-        blue: "bg-blue-400",
-        navy: "bg-blue-900",
-        red: "bg-red-400",
+        yellow: 'bg-yellow-400',
+        green: 'bg-green-400',
+        blue: 'bg-blue-400',
+        navy: 'bg-blue-900',
+        red: 'bg-red-400',
       };
-      const posisiColor = colorMap[row.original.posisiColor] ?? "bg-gray-400";
-      const mutation = api.lembaga.removeAnggota.useMutation()
-      const onDelete = (id: string) => {
-        mutation.mutate({user_id: id}, {
+      const posisiColor = colorMap[row.original.posisiColor] ?? 'bg-gray-400';
+      const mutation = api.event.removePanitia.useMutation();
+
+      const onDelete = (id: string, event_id: string) => {
+        mutation.mutate(
+          { id: id, event_id: event_id },
+          {
             onSuccess: () => {
-                mutation.reset()
-                window.location.reload()
+              mutation.reset();
+              window.location.reload();
             },
-        })
-      }
+          },
+        );
+      };
 
       return (
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
             <span className={`h-2 w-2 rounded-full ${posisiColor}`}></span>
-            {row.getValue("posisi")}
+            {row.getValue('posisi')}
           </div>
           <Dialog>
             <DialogTrigger asChild>
               <Button
-                  // onClick={() => onDelete(row.original.id)}
-                  variant={"outline"} size={"sm"} className="border-red-400 text-red-400 hover:border-red-500 hover:text-red-500">
+                // onClick={() => onDelete(row.original.id)}
+                variant={'outline'}
+                size={'sm'}
+                className="border-red-400 text-red-400 hover:border-red-500 hover:text-red-500"
+              >
                 Hapus
               </Button>
             </DialogTrigger>
             <DialogContent>
               <DialogHeader>
                 <DialogTitle>Hapus Anggota</DialogTitle>
-                <DialogDescription>Apakah kamu yakin ingin menghapus anggota ini?</DialogDescription>
+                <DialogDescription>
+                  Apakah kamu yakin ingin menghapus anggota ini?
+                </DialogDescription>
               </DialogHeader>
               <div className="w-full flex items-center justify-center gap-x-4">
-                <Button>
-                  Tidak, Batalkan
-                </Button>
-                <Button onClick={() => onDelete(row.original.id)}
-                    variant={"destructive"}>
+                <Button>Tidak, Batalkan</Button>
+                <Button
+                  onClick={() =>
+                    onDelete(row.original.id, row.original.event_id)
+                  }
+                  variant={'destructive'}
+                >
                   Ya, Hapus
                 </Button>
               </div>
@@ -114,15 +127,12 @@ const columns: ColumnDef<Member>[] = [
   },
 ];
 
-export function MahasiswaCardTable(
-    {
-        data
-    } : {
-        data: Member[]
-    }
-) {
+export function MahasiswaKegiatanCardTable({
+  data,
+}: {
+  data: MemberKegiatan[];
+}) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
-
   const table = useReactTable({
     data: data,
     columns,
@@ -141,10 +151,16 @@ export function MahasiswaCardTable(
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id} className="text-left text-gray-600">
+                  <TableHead
+                    key={header.id}
+                    className="text-left text-gray-600"
+                  >
                     {header.isPlaceholder
                       ? null
-                      : flexRender(header.column.columnDef.header, header.getContext())}
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
                   </TableHead>
                 ))}
               </TableRow>
@@ -156,7 +172,10 @@ export function MahasiswaCardTable(
                 <TableRow key={row.id}>
                   {row.getVisibleCells().map((cell) => (
                     <TableCell key={cell.id} className="text-gray-800">
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
                     </TableCell>
                   ))}
                 </TableRow>
