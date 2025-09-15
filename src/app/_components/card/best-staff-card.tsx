fa485b4976576aa7af3ad43ae1d3bb4facddc921
TukangLas21 <19623160@std.stei.itb.ac.id>
2025-09-15 23:40:07 +0700

    fix: profile picture container and paddings

--- src/app/_components/card/best-staff-card.tsx
+++ src/app/_components/card/best-staff-card.tsx
@@ -2,6 +2,8 @@ import Image, { type StaticImageData } from "next/image";
 import Link from "next/link";
 import dummyProfile from 'public/images/placeholder/profile-pic.png';
 import { Card } from "~/components/ui/card";
+import { getServerAuthSession } from '~/server/auth';
+import YellowStarIcon from 'public/icons/yellow-star.svg';
 
 // const dummyData = {
 //   nama: 'John Doe',
@@ -10,14 +12,6 @@ import { Card } from "~/components/ui/card";
 //   profilePhoto: dummyProfile,
 //   divisi: 'UI/UX'
 // }
-
-const YellowStarIcon = (props) => {
-  return (
-    <svg width="36" height="33" viewBox="0 0 36 33" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path d="M18 0L22.0413 12.4377H35.119L24.5389 20.1246L28.5801 32.5623L18 24.8754L7.41987 32.5623L11.4611 20.1246L0.880983 12.4377H13.9587L18 0Z" fill="#F5CB69"/>
-    </svg>
-  )
-}
 
 interface BestStaffCardProps {
   nama: string;
@@ -28,7 +22,7 @@ interface BestStaffCardProps {
   id_mahasiswa: string;
 }
 
-export default function BestStaffCard({
+export default async function BestStaffCard({
   nama = 'John Doe',
   NIM = '111234567',
   jurusan = 'Sastra Mesin',
@@ -36,27 +30,44 @@ export default function BestStaffCard({
   divisi = 'UI/UX',
   id_mahasiswa = '1'
 } : BestStaffCardProps) {
+  const session = await getServerAuthSession();
+  const isLembaga = session?.user.role === 'lembaga';
+
+  const namaSliced = nama.length > 22 ? nama.slice(0, 21) + '...' : nama;
+  const divisiSliced = divisi.length > 28 ? divisi.slice(0, 24) + '...' : divisi;
+
   return (
-    <Link href={`/lembaga/profile-mahasiswa/${id_mahasiswa}`} className="no-underline">
-      <Card className="w-60 h-[200px] rounded-[20px] bg-white hover:bg-[#00B7B7] group border border-[#E0E5E8] flex flex-col items-center justify-center hover:cursor-pointer">
+    <Link 
+      href={isLembaga ? `/lembaga/profile-mahasiswa/${id_mahasiswa}` : `/profile-mahasiswa/${id_mahasiswa}`} 
+      className="no-underline"
+    >
+      <Card className="w-60 min-h-[200px] rounded-[20px] bg-white hover:bg-[#00B7B7] group border border-[#E0E5E8] flex flex-col items-center justify-center hover:cursor-pointer">
         <div className="w-full h-full flex">
           <div className="relative w-full h-full flex flex-col items-center justify-center">
             <div className="absolute top-[17px] left-[192px]">
-              <YellowStarIcon />
-            </div>
-            <div className="flex flex-col items-center justify-center gap-y-[9px]">
               <Image 
-                src={profilePicture}
-                alt="Profile Picture"
-                width={64}
-                height={64}
-                className="rounded-full object-cover border border-[#009292] group-hover:border-[#E0E5E8]"
+                src={YellowStarIcon}
+                alt="Yellow Star Icon"
+                width={36}
+                height={36}
+                className="object-cover"
               />
+            </div>
+            <div className="flex flex-col items-center justify-center px-1 py-[27px]">
+              <div className="w-16 h-16 rounded-full overflow-hidden border border-[#009292] group-hover:border-[#E0E5E8] mb-[9px]">
+                <Image 
+                  src={profilePicture}
+                  alt="Profile Picture"
+                  width={64}
+                  height={64}
+                  className="object-cover w-full h-full"
+                />
+              </div>
 
               <div className="text-center">
-                <h1 className="font-bold text-lg text-[#009292] group-hover:text-white">{nama}</h1>
+                <h1 className="font-bold text-lg text-[#009292] group-hover:text-white">{namaSliced}</h1>
                 <p className="text-[10px] text-[#141718] group-hover:text-white">{NIM} - {jurusan}</p>
-                <h2 className="text-[16px] text-[#9DA4A8] font-semibold group-hover:text-white">{divisi}</h2>
+                <h2 className="text-[16px] text-[#9DA4A8] font-semibold group-hover:text-white">{divisiSliced}</h2>
               </div>
             </div>
           </div>
