d99d3d3b4a9d04b8e8330c1a598cd63e1ae26f41
angkaberapa <angkawijayamichael@gmail.com>
2025-09-18 09:26:13 +0700

    feat: api docs page for development

---
+++ src/app/api/panel/route.ts
@@ -0,0 +1,15 @@
+import { renderTrpcPanel } from '@metamorph/trpc-panel';
+import { appRouter } from '~/server/api/root';
+
+export async function GET() {
+  return new Response(
+    renderTrpcPanel(appRouter, {
+      url: 'http://localhost:3000/api/trpc',
+      transformer: 'superjson',
+    }),
+    {
+      status: 200,
+      headers: [['Content-Type', 'text/html']],
+    },
+  );
+}
