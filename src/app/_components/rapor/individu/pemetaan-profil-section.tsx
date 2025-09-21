21a08fa5ea038e53a536c54d5ff071cf9c046cf6
TukangLas21 <19623160@std.stei.itb.ac.id>
2025-09-21 20:06:17 +0700

    feat: change detail pemetaan section to accordion

--- src/app/_components/rapor/individu/pemetaan-profil-section.tsx
+++ src/app/_components/rapor/individu/pemetaan-profil-section.tsx
@@ -1,20 +1,26 @@
-import ProfilTable from "~/app/lembaga/kegiatan/_components/table/event-profil-table"
-import { ChevronDown } from "lucide-react"
-import { eventProfilProp } from "~/app/lembaga/kegiatan/[kegiatanId]/profil/constant"
-import { PemetaanProfilSectionProps } from "~/app/lembaga/kegiatan/[kegiatanId]/panitia/[raporId]/page"
+import { type PemetaanProfilSectionProps } from '~/app/lembaga/kegiatan/[kegiatanId]/panitia/[raporId]/page';
+import { eventProfilProp } from '~/app/lembaga/kegiatan/[kegiatanId]/profil/constant';
+import ProfilTable from '~/app/lembaga/kegiatan/_components/table/event-profil-table';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '~/components/ui/accordion';
 
-export default function PemetaanProfilSection({
+export default function DetailPemetaanProfilSection({
   pemetaanProfilData = eventProfilProp,
-} : PemetaanProfilSectionProps) {
+}: PemetaanProfilSectionProps) {
   return (
-    <div className="flex flex-col gap-[10px]">
-      <div className="flex flex-row gap-[5px] items-center">
-        <div className="text-[20px]/8 text-neutral-700">
-          Detail Pemetaan Profil Kegiatan
-        </div>
-        <ChevronDown className="w-6 h-6 text-neutral-700" />
-      </div>
-      <ProfilTable profilData={pemetaanProfilData} />
-    </div>
-  )
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger className="text-[20px]/8 text-neutral-700 mb-[10px] font-normal no-underline">
+          Pemetaan Profil Kegiatan
+        </AccordionTrigger>
+        <AccordionContent>
+          <ProfilTable profilData={pemetaanProfilData} />
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
 }
