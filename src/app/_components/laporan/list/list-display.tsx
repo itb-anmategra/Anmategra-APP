d2700d7f53565756eaddd34e438b5d1a40bda99b
parkuskus <ksmaachmad@gmail.com>
2025-09-24 09:28:14 +0700

    fix: fix ListDisplay

--- src/app/_components/laporan/list/list-display.tsx
+++ src/app/_components/laporan/list/list-display.tsx
@@ -1,6 +1,6 @@
-import { type KanbanBoardProps } from "../board/kanban-board";
-import { getTypeImage } from "../board/report-column";
-import { AccordionDisplay } from "./accordion-display";
+import { type KanbanBoardProps } from '../board/kanban-board';
+import { getTypeImage } from '../board/report-column';
+import { AccordionDisplay } from './accordion-display';
 
 export const ListDisplay = ({
   kanbanData,
@@ -8,15 +8,17 @@ export const ListDisplay = ({
 }: KanbanBoardProps) => {
   return (
     <div className="flex flex-col gap-2">
-      {kanbanData.map((data, index) => (
-        <AccordionDisplay
-          key={index}
-          logo={getTypeImage(data.title)}
-          title={data.title}
-          reports={data.reports}
-          selectedStatus={displayedColumn}
-        />
-      ))}
+      {kanbanData
+        .filter((data) => displayedColumn.includes(data.title))
+        .map((data, index) => (
+          <AccordionDisplay
+            key={index}
+            logo={getTypeImage(data.title)}
+            title={data.title}
+            reports={data.reports}
+            selectedStatus={displayedColumn}
+          />
+        ))}
     </div>
   );
 };
