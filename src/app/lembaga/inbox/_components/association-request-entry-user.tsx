ce7f7afc08e4a8c266ea130d3c2b560ef203dff2
max <13523031@std.stei.itb.ac.id>
2025-09-26 09:06:30 +0700

    [FE] feat: integrate association request

--- src/app/lembaga/inbox/_components/association-request-entry-user.tsx
+++ src/app/lembaga/inbox/_components/association-request-entry-user.tsx
@@ -2,7 +2,9 @@
 
 import Image from 'next/image';
 import React from 'react';
+import { type z } from 'zod';
 import { Button } from '~/components/ui/button';
+import { type GetAllRequestAssociationOutputSchema } from '~/server/api/types/lembaga.type';
 
 type PermintaanAsosiasi = {
   image: string;
@@ -11,66 +13,89 @@ type PermintaanAsosiasi = {
   divisi: string;
 };
 
-const AssociationRequestEntryUser: React.FC<{ data: PermintaanAsosiasi[] }> = ({
-  data,
-}) => {
+type cobaAPI = z.infer<typeof GetAllRequestAssociationOutputSchema>;
+
+const AssociationRequestEntryUser: React.FC<{ data: cobaAPI }> = ({ data }) => {
   return (
     <div className="flex flex-col rounded-xl pl-1 pb-1 p-4 font-sans">
-      <div className="grid grid-cols-[2fr_1fr_1fr_1.5fr] items-center gap-4 border-b border-[#E7E9EC] p-4 font-regular weight-400 text-[18px] text-[#9DA4A8]">
-        <div>Nama</div>
-        <div className="pl-[50px]">Posisi</div>
-        <div className="text-center">Divisi</div>
-        <div className="justify-self-end mr-[110px]">Request</div>
-      </div>
+      {Array.isArray(data) && data.length > 0 ? (
+        <>
+          <div className="grid grid-cols-[2fr_1fr_1fr_1.5fr] items-center gap-4 border-b border-[#E7E9EC] p-4 font-regular weight-400 text-[18px] text-[#9DA4A8]">
+            <div>Nama</div>
+            <div className="pl-[50px]">Posisi</div>
+            <div className="text-center">Divisi</div>
+            <div className="justify-self-end mr-[110px]">Request</div>
+          </div>
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className={
+                'grid grid-cols-[2fr_1fr_1fr_1.5fr] items-center gap-4 p-4 border-b border-[#E7E9EC]'
+              }
+            >
+              <div className="flex items-center gap-4">
+                <div className="rounded-full overflow-hidden w-[60px] h-[60px]">
+                  <Image
+                    src={'/images/miscellaneous/empty-profile-picture.svg'}
+                    alt="Profile Picture"
+                    width={60}
+                    height={60}
+                  />
+                </div>
+                <span className="pl-[20px] text-[18px] font-regular weight-400 text-[#636A6D]">
+                  {item?.mahasiswa_name ?? '-'}
+                </span>
+              </div>
+              <div className="text-[18px] font-regular weight-400 pl-[50px] text-[#636A6D]">
+                {item?.position ?? '-'}
+              </div>
+              <div className="text-[18px] font-regular weight-400 text-center text-[#636A6D]">
+                {item?.division ?? '-'}
+              </div>
+              <div className="flex items-center justify-end gap-2 weight-700">
+                <Button
+                  onClick={() =>
+                    console.log(
+                      `Permintaan untuk divisi ${item?.division ?? '-'} ditolak.`,
+                    )
+                  }
+                  className="border-none px-4 py-2 bg-[#FAFAFA] text-[14px] text-[#FF0000] hover:bg-[#FF0000] hover:text-white active:bg-[#FF0000] active:text-white"
+                >
+                  DECLINE
+                </Button>
 
-      {data.map((item: PermintaanAsosiasi, index: number) => (
-        <div
-          key={index}
-          className={
-            'grid grid-cols-[2fr_1fr_1fr_1.5fr] items-center gap-4 p-4 border-b border-[#E7E9EC]'
-          }
-        >
-          <div className="flex items-center gap-4">
-            <div className="rounded-full overflow-hidden w-[60px] h-[60px]">
-              <Image
-                src={item.image}
-                alt="Profile Picture"
-                width={60}
-                height={60}
-              />
+                <Button
+                  onClick={() =>
+                    console.log(
+                      `Permintaan untuk divisi ${item?.division ?? '-'} diterima.`,
+                    )
+                  }
+                  variant="outline"
+                  className="rounded-2px border-[#29BC5B] border-[2px] px-4 py-2 bg-[#FAFAFA] text-[14px] text-[#29BC5B] hover:bg-[#29BC5B] hover:text-white"
+                >
+                  ACCEPT
+                </Button>
+              </div>
             </div>
-            <span className="pl-[20px] text-[18px] font-regular weight-400 text-[#636A6D]">
-              {item.nama}
-            </span>
+          ))}
+        </>
+      ) : (
+        <>
+          <Image
+            src="/images/miscellaneous/not-found-association.svg"
+            alt="No Data"
+            width={140}
+            height={140}
+            className="mx-auto mt-20"
+          />
+          <div className="p-4 pb-0 text-center weight-600 font-semibold text-[32px] text-[#768085]">
+            Tidak ada permintaan asosiasi
           </div>
-          <div className="text-[18px] font-regular weight-400 pl-[50px] text-[#636A6D]">
-            {item.posisi}
-          </div>
-          <div className="text-[18px] font-regular weight-400 text-center text-[#636A6D]">
-            {item.divisi}
-          </div>
-          <div className="flex items-center justify-end gap-2 weight-700">
-            <Button
-              onClick={() =>
-                console.log(`Permintaan untuk divisi ${item.divisi} ditolak.`)
-              }
-              className="border-none px-4 py-2 bg-[#FAFAFA] text-[14px] text-[#FF0000] hover:bg-[#FF0000] hover:text-white active:bg-[#FF0000] active:text-white"
-            >
-              DECLINE
-            </Button>
-
-            <Button
-              onClick={() =>
-                console.log(`Permintaan untuk divisi ${item.divisi} diterima.`)
-              }
-              variant="outline"
-              className="rounded-2px border-[#29BC5B] border-[2px] px-4 py-2 bg-[#FAFAFA] text-[14px] text-[#29BC5B] hover:bg-[#29BC5B] hover:text-white"
-            >
-              ACCEPT
-            </Button>
-          </div>
-        </div>
-      ))}
+          <p className="p-4 pt-0 text-center weight-600 font-regular text-[24px] text-[#C4CACE]">
+            Maaf, belum ada permintaan asosiasi yang masuk
+          </p>
+        </>
+      )}
     </div>
   );
 };
