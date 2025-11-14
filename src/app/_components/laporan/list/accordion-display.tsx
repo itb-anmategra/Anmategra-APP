84bf65b291b6ec037a7b1fbfc48249689b1a7c2e
Daniel <19624107@mahasiswa.itb.ac.id>
2025-11-15 06:30:54 +0700

    feat: integrate laporan lembaga

--- src/app/_components/laporan/list/accordion-display.tsx
+++ src/app/_components/laporan/list/accordion-display.tsx
@@ -56,7 +56,7 @@ export const AccordionDisplay = ({
               </div>
 
               {/* Kanan: Tombol Buat Laporan */}
-              {!isAdminView && <LaporanFormDialog />}
+              {!isAdminView && <LaporanFormDialog isAdmin={isAdminView ?? false} />}
             </div>
           </div>
         </div>
