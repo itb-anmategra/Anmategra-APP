6f8c60428cb1517f77f65f5cfcd3d2f0a35943b5
Albertus Christian Poandy <163734409+albertchriss@users.noreply.github.com>
2026-01-03 19:56:41 +0900

    feat: app on maintenance (#157)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,53 @@
-import "~/styles/globals.css";
 // Font Import
-import {GeistSans} from "geist/font/sans";
+import { GeistSans } from 'geist/font/sans';
 // Library Import
-import {type Metadata} from "next";
-import {TRPCReactProvider} from "~/trpc/react";
-import Image from "next/image";
+import { type Metadata } from 'next';
+import Image from 'next/image';
+import Kabinet from 'public/images/kabinet.png';
 // Image Import
-import LogoAnmategra from "public/images/logo-anmategra.png";
-import Mascot from "public/images/mascot.png";
-import Kabinet from "public/images/kabinet.png";
+import LogoAnmategra from 'public/images/logo-anmategra.png';
+import Mascot from 'public/images/mascot.png';
 // Components Import
-import { Toaster } from "~/components/ui/toaster"
-import Footer from "./_components/Footer";
+import { Toaster } from '~/components/ui/toaster';
+import '~/styles/globals.css';
+import { TRPCReactProvider } from '~/trpc/react';
+
+import Footer from './_components/Footer';
+
 // Metadata
 export const metadata: Metadata = {
-  title: "Anmategra",
-  description: "Anmategra by KM ITB",
-  icons: [{ rel: "icon", url: "/images/favicon.ico" }],
+  title: 'Anmategra',
+  description: 'Anmategra by KM ITB',
+  icons: [{ rel: 'icon', url: '/images/favicon.ico' }],
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-      <html lang="en" className={`${GeistSans.variable}`}>
-        <body className="bg-neutral-100 overflow-auto">
-          <div className="min-h-screen flex flex-col hidden lg:block">
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body className="bg-neutral-100 overflow-auto">
+        <div className="w-full h-screen flex flex-col justify-center relative">
+          <div className="absolute flex items-center justify-center py-12 top-0 left-0 right-0">
+            <Image
+              src={LogoAnmategra}
+              alt="Logo Anmategra"
+              width={210}
+              height={112}
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src={Mascot} alt="Mascot" width={240} height={180} />
+            <h1 className="text-[#00B7B7] text-xl font-bold">
+              Sedang Dalam Pengembangan
+            </h1>
+            <h2 className="text-center text-md">
+              Aplikasi ini masih dalam tahap pengembangan. <br />
+              Kami sedang bekerja keras untuk segera menghadirkannya untuk Anda.
+            </h2>
+          </div>
+        </div>
+        {/* <div className="min-h-screen flex flex-col hidden lg:block">
             <div className="flex-1">
               <TRPCReactProvider>{children}</TRPCReactProvider>
             </div>
@@ -61,8 +83,8 @@ export default function RootLayout({
               />
             </div>
           </div>
-          <Toaster />
-        </body>
-      </html>
+          <Toaster /> */}
+      </body>
+    </html>
   );
 }
