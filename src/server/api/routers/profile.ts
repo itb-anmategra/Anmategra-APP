8dfec4d3b28f7434a1aaa90b83ea9a27d7ebdbd7
albertchriss <19623210@std.stei.itb.ac.id>
2026-02-01 14:32:56 +0800

    feat: public detail kegiatan and lembaga pages

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,6 +1,10 @@
 import { TRPCError } from '@trpc/server';
 import { and, desc, eq } from 'drizzle-orm';
-import { createTRPCRouter, protectedProcedure, publicProcedure } from '~/server/api/trpc';
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from '~/server/api/trpc';
 import {
   events,
   keanggotaan,
@@ -14,8 +18,10 @@ import { type Kepanitiaan } from '~/types/kepanitiaan';
 import {
   GetKegiatanInputSchema,
   GetKegiatanOutputSchema,
+  GetKegiatanPublicOutputSchema,
   GetLembagaInputSchema,
   GetLembagaOutputSchema,
+  GetLembagaPublicOutputSchema,
   GetMahasiswaInputSchema,
   GetMahasiswaOutputSchema,
 } from '../types/profile.type';
@@ -96,7 +102,7 @@ export const profileRouter = createTRPCRouter({
       };
     }),
 
-  getLembaga: publicProcedure
+  getLembaga: protectedProcedure
     .input(GetLembagaInputSchema)
     .output(GetLembagaOutputSchema)
     .query(async ({ ctx, input }) => {
@@ -199,7 +205,7 @@ export const profileRouter = createTRPCRouter({
       };
     }),
 
-  getKegiatan: publicProcedure
+  getKegiatan: protectedProcedure
     .input(GetKegiatanInputSchema)
     .output(GetKegiatanOutputSchema)
     .query(async ({ ctx, input }) => {
@@ -281,6 +287,126 @@ export const profileRouter = createTRPCRouter({
         kegiatan: kegiatan,
         lembaga: lembagaRes[0],
         participant: formattedParticipants,
+      };
+    }),
+
+  getLembagaPublic: publicProcedure
+    .input(GetLembagaInputSchema)
+    .output(GetLembagaPublicOutputSchema)
+    .query(async ({ ctx, input }) => {
+      const lembaga = await ctx.db.query.lembaga.findFirst({
+        where: (lembaga, { eq }) => eq(lembaga.id, input.lembagaId),
+        with: {
+          users: {
+            columns: {
+              id: true,
+              image: true,
+            },
+          },
+        },
+        columns: {
+          id: true,
+          name: true,
+          description: true,
+          memberCount: true,
+          foundingDate: true,
+          endingDate: true,
+          type: true,
+          raporVisible: true,
+        },
+      });
+
+      if (!lembaga) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Lembaga tidak ditemukan.',
+        });
+      }
+
+      const newestEvent = await ctx.db.query.events.findMany({
+        where: (events, { eq }) => and(eq(events.org_id, input.lembagaId)),
+        orderBy: desc(events.start_date),
+      });
+
+      const formattedEvents: Kepanitiaan[] = newestEvent.map((item) => ({
+        lembaga: {
+          id: lembaga.id,
+          name: lembaga.name,
+          profilePicture: lembaga.users.image,
+        },
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        anggotaCount: item.participant_count ?? 0,
+        image: item.background_image,
+        startDate: new Date(item.start_date),
+        endDate: item.end_date ? new Date(item.end_date) : null,
+      }));
+
+      const highlightedEvent = await ctx.db.query.events.findFirst({
+        where: (events, { eq }) =>
+          and(
+            eq(events.org_id, input.lembagaId),
+            eq(events.is_highlighted, true),
+          ),
+        orderBy: desc(events.start_date),
+      });
+
+      return {
+        lembagaData: lembaga,
+        newestEvent: formattedEvents,
+        highlightedEvent: highlightedEvent ? highlightedEvent : null,
+      };
+    }),
+
+  getKegiatanPublic: publicProcedure
+    .input(GetKegiatanInputSchema)
+    .output(GetKegiatanPublicOutputSchema)
+    .query(async ({ ctx, input }) => {
+      const kegiatan = await ctx.db.query.events.findFirst({
+        where: (events, { eq }) => eq(events.id, input.kegiatanId),
+      });
+
+      if (!kegiatan) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Kegiatan tidak ditemukan.',
+        });
+      }
+
+      if (kegiatan.org_id === null) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Kegiatan tidak ditemukan.',
+        });
+      }
+
+      const lembagaRes = await ctx.db
+        .select({
+          id: lembaga.id,
+          name: lembaga.name,
+          description: lembaga.description,
+          image: users.image,
+          memberCount: lembaga.memberCount,
+          foundingDate: lembaga.foundingDate,
+          endingDate: lembaga.endingDate,
+          type: lembaga.type,
+        })
+        .from(lembaga)
+        .innerJoin(users, eq(lembaga.userId, users.id))
+        .where(eq(lembaga.id, kegiatan.org_id))
+        .limit(1);
+
+      if (lembagaRes.length === 0 || !lembagaRes[0]) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Lembaga tidak ditemukan.',
+        });
+      }
+
+      return {
+        kegiatan: kegiatan,
+        lembaga: lembagaRes[0],
       };
     }),
 });
